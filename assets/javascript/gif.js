cf98eace2f70702542efc95834d258076b17c944
Erin Giannette <emg4784@yahoo.com>
2016-09-04 18:51:26 -0500

    added for loop and btns

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,16 +1,15 @@
+var topics = ["bobs burgers", "golden girls", "scooby doo"];
+
+for(var i=0; i< topics.length;i++){
+    var button = $('<input type="button" value="' + topics[i] + '" + data-show/>');
+    $("#buttons").append(button);
+    
+
+}
+
+
+
 $('#buttons').on('click', function() {
+
+});
        
-        // After the click function it accesses api and searches for cats
-        var queryURL = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=beaarthur";
-
-        // Going to the URL and getting the data; 
-        $.ajax({url: queryURL, method: 'GET'})
-
-            // When its finished getting the query. 
-            .done(function(response) {
-
-                // Setting the variable of image_url to the specific object 
-                var imageUrl = response.data.image_original_url;
-
-                // Created a variable called cat image
-                var catImage = $("<img>");
