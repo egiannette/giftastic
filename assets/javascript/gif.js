b31c14724945e6dda4931578c2e40e4bcf0622a9
Erin Giannette <emg4784@yahoo.com>
2016-09-04 19:13:48 -0500

    adding code for getting gifs

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,7 +1,8 @@
 var topics = ["bobs burgers", "golden girls", "scooby doo"];
+var queryURL = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=";
 
 for(var i=0; i< topics.length;i++){
-    var button = $('<input type="button" value="' + topics[i] + '" + data-show/>');
+    var button = $('<input type="button" value="' + topics[i] + '" + data-show class="showImage" />');
     $("#buttons").append(button);
     
 
@@ -9,7 +10,28 @@ for(var i=0; i< topics.length;i++){
 
 
 
-$('#buttons').on('click', function() {
+$('.showImage').on('click', function() {
+    var myQueryUrl = queryURL + $(this).attr('value');
+    console.log(myQueryUrl);
+
+    $.ajax({url: myQueryUrl, method: 'GET'})
+
+            // When its finished getting the query. 
+            .done(function(response) {
+
+                // Setting the variable of image_url to the specific object 
+                var imageUrl = response.data.image_original_url;
+
+                // Created a variable called show image
+                var showImage = $("<img>");
+                
+                // Given the catImage the source path and an alt tag (screen readers). 
+                showImage.attr('src', imageUrl);
+                showImage.attr('alt', 'show image');
+
+                // Adding images before the content that's there 
+                $('#gifsGoHere').prepend(showImage);
+            });
 
 });
        
